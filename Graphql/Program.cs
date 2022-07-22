577c7f838c35681b78a70a84f6c31728f781f9f0
Adrian U <adrian@bsd-net.pl>
2022-07-22 08:27:34 +0200

    fix-22-07-2022

--- Graphql/Program.cs
+++ Graphql/Program.cs
@@ -4,40 +4,37 @@ using Graphql.schema.Subscription;
 using Graphql.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//private readonly IConfiguration _configuration;
-//public Program(IConfiguration configuration)
-//{
-//    _configuration = configurationl
-//}
+//builder.Services.AddDbContextFactory<SchoolDbContext>(o => o.UseSqlite("Data Source=recipes.sqlite3"));
+//builder.Services.AddScoped<SchoolDbContext>(
+//    sp => sp.GetRequiredService<IDbContextFactory<SchoolDbContext>>()
+//    .CreateDbContext());
 
-IHost host = builder.Build();
-using (IServiceScope scope = host.Services.CreateScope())
-{
-    IDbContextFactory<SchoolDbContext> contextFactory = 
-        scope.ServiceProvider.GetRequiredService<IDbContextFactory<SchoolDbContext>>();
-    using(SchoolDbContext context = contextFactory.CreateDbContext())
-    {
-        context.Database.Migrate();
-    }
-}
-
-    host.Start();
+string connectionString = builder.Configuration.GetConnectionString("default");
+builder.Services.AddPooledDbContextFactory<SchoolDbContext>(o => o.UseSqlite(connectionString));
 
 builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>()
     .AddMutationType<Mutation>()
-    .AddSubscriptionType<Subscription>();
-   // .RegisterDbContext<SchoolDbContext>(DbContextKind.Pooled);
+    .AddSubscriptionType<Subscription>()
+    .RegisterDbContext<SchoolDbContext>(DbContextKind.Pooled);
 
 builder.Services.AddInMemorySubscriptions();
-string connectionString = builder.Configuration.GetConnectionString("default");
-builder.Services.AddPooledDbContextFactory<SchoolDbContext>(o => o.UseSqlite(connectionString));
+
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+
+using var contextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<SchoolDbContext>>().CreateDbContext();
+
+contextFactory.Database.Migrate();
+    
+
 app.UseRouting();
 app.UseWebSockets();  //subscription
 app.MapGraphQL();
