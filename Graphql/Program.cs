ac5cedc04baa02245b18307b81163b7d595833f5
Adrian U <adrian@bsd-net.pl>
2022-07-22 08:42:32 +0200

    fix-22-07-2022-b

--- Graphql/Program.cs
+++ Graphql/Program.cs
@@ -8,10 +8,6 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddDbContextFactory<SchoolDbContext>(o => o.UseSqlite("Data Source=recipes.sqlite3"));
-//builder.Services.AddScoped<SchoolDbContext>(
-//    sp => sp.GetRequiredService<IDbContextFactory<SchoolDbContext>>()
-//    .CreateDbContext());
 
 string connectionString = builder.Configuration.GetConnectionString("default");
 builder.Services.AddPooledDbContextFactory<SchoolDbContext>(o => o.UseSqlite(connectionString));
@@ -20,21 +16,17 @@ builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>()
     .AddMutationType<Mutation>()
-    .AddSubscriptionType<Subscription>()
-    .RegisterDbContext<SchoolDbContext>(DbContextKind.Pooled);
+    .AddSubscriptionType<Subscription>();
+    //.RegisterDbContext<SchoolDbContext>(DbContextKind.Pooled);
 
 builder.Services.AddInMemorySubscriptions();
-
 
 var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
-
 using var contextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<SchoolDbContext>>().CreateDbContext();
-
 contextFactory.Database.Migrate();
     
-
 app.UseRouting();
 app.UseWebSockets();  //subscription
 app.MapGraphQL();
